b713d51274a64b03255a08959928e9f1f0b52213
wu jian <wujian016@163.com>
2014-11-09 22:10:13 +0800

    create new branch named dev

--- jq_1.js
+++ jq_1.js
@@ -2,5 +2,6 @@
 var jsd = {};
 jsd.version = "1.283.3"; 
 jsd.sex = "m";
-jsd.max = 1292.3;
-jsd.fam = "028-288383";
+jsd.max = 1292.3; 
+jsd.fam = "028-288383"; 
+jsd.num = 8099; 
--- jsd.js
+++ jsd.js
@@ -1,6 +1,10 @@
 getJSONP.cb0( 
  {"name":"wufeie","year":99,amount:399,
-  date:"2014-3-12","sonumber":77880,
+  date:"2014-3-12","sonumber":77880, 
   sex:"F",
-  man:"good"} 
+  man:"good"
+  sex:"m",
+  man:"good",
+  num:200192
+} 
 )
